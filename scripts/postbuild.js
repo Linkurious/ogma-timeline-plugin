b26befd556de673b2e7397de7db98629cbeca998
Leo Nicolle <leo@linkurio.us>
2023-03-01 16:35:30 +0100

    generate package.json in dist

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -5,15 +5,21 @@ const package = require("../package.json");
 
 const ogmaVersion = package.dependencies["@linkurious/ogma"];
 
+["main", "module", "types"].forEach((key) => {
+  package[key] = package[key].replace("dist", ".");
+});
+package.files = package.files.map((filename) => filename.replace("dist", "."));
 package.devDependencies = {};
 delete package.dependencies["@linkurious/ogma"];
 delete package.scripts;
 package.peerDependencies["@linkurious/ogma"] = ogmaVersion;
+
 fs.writeFile(
-  path.resolve("package.json"),
+  path.resolve("dist/package.json"),
   JSON.stringify(package, 0, 2),
   "utf-8",
   (err) => {
-    console.log("wirte", err);
+    if (!err) return;
+    console.log("Error writing JSON", err);
   }
 );
