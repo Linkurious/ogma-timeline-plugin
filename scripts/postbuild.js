122bd9329ac50f12b4cba12c7ab7562cf2fe36ca
Alexander Milevski <alexander@linkurio.us>
2023-03-02 09:56:27 +0100

    simplify

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -5,19 +5,15 @@ fs.readFile("package.json", { encoding: "utf-8" })
   .then((file) => {
     const pkg = JSON.parse(file);
     const ogmaVersion = pkg.dependencies["@linkurious/ogma"];
-    ["main", "module", "types"].forEach((key) => {
-      pkg[key] = pkg[key].replace("dist", ".");
-    });
-    pkg.files = pkg.files.map((filename) => filename.replace("dist", "."));
     pkg.devDependencies = {};
     delete pkg.dependencies["@linkurious/ogma"];
     delete pkg.engines;
     delete pkg.scripts;
     pkg.peerDependencies["@linkurious/ogma"] = ogmaVersion;
-    return fs.writeFile(
-      path.resolve("dist/package.json"),
-      JSON.stringify(pkg, 0, 2),
-      "utf-8"
-    );
+    // return fs.writeFile(
+    //   path.resolve("dist/package.json"),
+    //   JSON.stringify(pkg, 0, 2),
+    //   "utf-8"
+    // );
   })
   .catch((err) => console.log("Error writing JSON", err));
