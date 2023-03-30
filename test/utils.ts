d1a7f7889dde6ad623a54f05af0cc17b03adf9b7
Leo Nicolle <leo@linkurio.us>
2023-03-30 17:15:43 +0200

    loosen even more

--- test/utils.ts
+++ test/utils.ts
@@ -55,7 +55,6 @@ export function compareDates(date1: Date, date2: Date) {
   return (
     date1.getFullYear() === date2.getFullYear() &&
     date1.getMonth() === date2.getMonth() &&
-    date1.getDate() === date2.getDate() &&
-    date1.getHours() === date2.getHours()
+    date1.getDate() === date2.getDate()
   );
 }
