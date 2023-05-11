b489806dfbe6f27108547c2e29b80b88073cb27b
Leo Nicolle <leo@linkurio.us>
2023-05-11 16:09:00 +0200

    no console log

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,11 +14,14 @@ module.exports = {
     sourceType: "module",
   },
   settings: {
-    "html/html-extensions": [".html", ".we"],  // consider .html and .we files as HTML
+    "html/html-extensions": [".html", ".we"], // consider .html and .we files as HTML
   },
   globals: {
     document: true,
     window: true,
+  },
+  rules: {
+    "no-console": 2,
   },
   plugins: ["promise", "import-order", "jsdoc", "html"],
 };
